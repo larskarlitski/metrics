e91be047dd080cf27b1625cc8c612fcd346cd66e
Lars Karlitski <lars@karlitski.net>
2023-01-01 19:32:04 +0000

    Add markdown report that can be rendered with madore
    An alternative way to create these reports, which might make creating
    them regularly easier (git-based workflow).
    
    Render with
    
        madore -o 2022-11.html --style redhat.css 2022-11.md
    
    [1] https://github.com/larskarlitski/madore

--- ibmetrics/__init__.py
+++ ibmetrics/__init__.py
@@ -1,1 +1,34 @@
 from . import data, metrics, plot, reader
+
+import pandas
+
+
+def load(dump_file, subscriptions_file, user_info_file=None, user_filter_file=None, start_date=None, end_date=None):
+    builds = reader.read_dump(dump_file)
+
+    users = None
+    if user_info_file:
+        users = pandas.read_json(user_info_file, dtype=False)
+
+    user_filter = []
+    if user_filter_file:
+        with open(user_filter_file, encoding="utf-8") as f:
+            user_filter = f.read().split("\n")
+
+    filter_ids = data.get_filter_ids(users, user_filter)
+
+    builds = data.slice_time(builds, start_date, end_date)
+    builds = data.filter_orgs(builds, filter_ids)
+
+    subscriptions = pandas.read_csv(
+        subscriptions_file,
+        delimiter="\t",
+        na_values=["None"],
+        parse_dates=[2, 3],
+        dtype={"org_id": str}
+    )
+    subscriptions = data.slice_time(subscriptions, start_date, end_date, field="created")
+    subscriptions = data.filter_orgs(subscriptions, filter_ids)
+    subscriptions = subscriptions.loc[subscriptions["element"] == "cloudapi-v2"]
+
+    return builds, subscriptions
